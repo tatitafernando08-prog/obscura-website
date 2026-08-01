f5cb3f2f36a6711b80e4476609296dbc3a826dad
tatitafernando08-prog <tatita.fernando08@gmail.com>
2026-08-01 21:09:41 +0530

    Assemble HomePage from marketing components

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,30 @@
+import { useScrollToHash } from '../hooks/useScrollToHash';
+import { Hero } from '../components/marketing/Hero';
+import { About } from '../components/marketing/About';
+import { Features } from '../components/marketing/Features';
+import { Plans } from '../components/marketing/Plans';
+import { NeshSection } from '../components/marketing/NeshSection';
+import { PomodoroTryout } from '../components/marketing/PomodoroTryout';
+import { RobotSection } from '../components/marketing/RobotSection';
+import { SnacksSection } from '../components/marketing/SnacksSection';
+import { Testimonials } from '../components/marketing/Testimonials';
+import { Contact } from '../components/marketing/Contact';
+
 export function HomePage() {
-  return <div>Home (WIP)</div>;
+  useScrollToHash();
+
+  return (
+    <>
+      <Hero />
+      <About />
+      <Features />
+      <Plans />
+      <NeshSection />
+      <PomodoroTryout />
+      <RobotSection />
+      <SnacksSection />
+      <Testimonials />
+      <Contact />
+    </>
+  );
 }
